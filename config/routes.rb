2960d0b1ba80d6f0b9b8681f7f17ec441ede56e7
John Cleary <john@createk.io>
2017-05-01 21:21:18 +0100

    routes for artifacts and collections via the slug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'pages#home'
+
+  resources :users, param: :username do
+    resources :artifacts, param: :slug
+    resources :collections, param: :slug
+  end
 end
