429dd0fe67aed23105c757edaa524abe7ebf4b69
John Cleary <john@createk.io>
2017-05-05 16:32:47 +0100

    add index and create actions

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -3,4 +3,19 @@ class ArtifactsController < ApplicationController
   def new
     @artifact = current_user.artifacts.build
   end
+
+  def create
+    current_user.artifacts.create!(artifact_params)
+    redirect_to action: :index
+  end 
+
+  def index
+    @artifacts = current_user.artifacts
+  end
+
+  private
+  
+  def artifact_param 
+    params.require(:artifact).permit(:name, :content)
+  end
 end
