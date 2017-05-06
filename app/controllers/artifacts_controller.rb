a893785c2e09cd27804e3f83c023f64e98e92482
John Cleary <john@createk.io>
2017-05-06 08:28:57 +0100

    add edit action for artifacts

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,21 +1,36 @@
 class ArtifactsController < ApplicationController
 
+  def index
+    @artifacts = current_user.artifacts
+  end
+
   def new
     @artifact = current_user.artifacts.build
   end
 
   def create
     current_user.artifacts.create!(artifact_params)
+    flash[:notice] = 'Artifact created.'
     redirect_to action: :index
   end 
 
-  def index
-    @artifacts = current_user.artifacts
+  def edit
+    @artifact = current_user.artifacts.find_by!(slug: params[:slug])
+  end
+
+  def update
+    @artifact = current_user.artifacts.find_by!(slug: params[:slug])
+    @artifact.update!(artifact_params)
+    flash[:notice] = 'Artifact updated.'
+    redirect_to action: :index
+  rescue ActiveRecord::RecordInvalid => invalid
+    @artifact = invalid.full_messages
+    render :edit
   end
 
   private
   
-  def artifact_param 
+  def artifact_params
     params.require(:artifact).permit(:name, :content)
   end
 end
--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -3,7 +3,15 @@ class Artifact < ApplicationRecord
   has_many :collection_artifacts, dependent: :destroy
   has_many :collections, through: :collection_artifacts
 
+  validates :name, uniqueness: { scope: :user_id, message: 'an article already exists with this title' }
+
+  before_save :default_slug
+
   def to_param
     slug
+  end
+
+  def default_slug
+    self.slug = name.parameterize
   end
 end
