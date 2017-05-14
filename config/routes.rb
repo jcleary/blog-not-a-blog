1f37eefec6822482af2e522abe73fad8ccbf8da6
John Cleary <john@createk.io>
2017-05-14 08:38:46 +0100

    create short routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,21 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'pages#home'
 
-  resources :users, param: :username do
+  resources :users, param: :username, only: [:show] do
     resources :artifacts, param: :slug
     resources :collections, param: :slug
   end
+
+  # short routes
+  #  - /username
+  #  - /username/artifact-slug
+  #  - /username/collections/collection-slug
+  #  - /username/tags/tag
+  get '*username/tags', to: 'tags#index', as: 'short_user_tags'
+  get '*username/tags/*tag', to: 'tags#show', as: 'short_user_tag'
+  get '*username/collections', to: 'collections#index', as: 'short_user_collections'
+  get '*username/collections/*slug', to: 'collections#show', as: 'short_user_collection'
+  get '*username/artifacts', to: 'artifacts#index', as: 'short_user_artifacts'
+  get '*username/*slug', to: 'artifacts#show', as: 'short_user_artifact'
+  get '*username', to: 'users#show', as: 'short_user' 
 end
