92026af09c4a21a966eb1651043e3007a77ec427
John Cleary <john@createk.io>
2017-05-26 16:02:32 +0100

    after sign in, redirect to the users home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   include CanCan::ControllerAdditions
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    short_user_path(resource)
+  end
 end
